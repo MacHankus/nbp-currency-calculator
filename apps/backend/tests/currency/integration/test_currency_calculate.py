11adea5bebf382f3cee3b401b51eef2549e486bf
Maciej Hankus <maciej.hankus0@gmail.com>
2023-12-01 23:35:13 +0100

    Added tests for currency

---
+++ apps/backend/tests/currency/integration/test_currency_calculate.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+
+import pytest
+from modules.currency.adapters.api.dto.incoming.calculate_currency_dto import (
+    IncomingCalculateCurrencyDTO,
+)
+from modules.currency.adapters.api.dto.outcoming.calculated_value_dto import (
+    CalculatedValueDTO,
+)
+from modules.currency.core.enums.currency_enum import CurrencyEnum
+from tests.testclient import client
+import itertools
+
+PATH = "/currency/calculate"
+
+
+def test_response_should_return_200_when_provided_all_good_params():
+    # Arrange
+    payload = IncomingCalculateCurrencyDTO(
+        currency_from="PLN", currency_to="USD", value=Decimal("1.1")
+    )
+
+    # Act
+    response = client.post(PATH, json=payload.model_dump_json())
+
+    # Assert
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(
+    "currency_from, currency_to", itertools.combinations(list(CurrencyEnum), 2)
+)
+def test_response_should_return_value_when_provided_all_good_params(
+    currency_from, currency_to
+):
+    # Arrange
+    payload = IncomingCalculateCurrencyDTO(
+        currency_from=currency_from,
+        currency_to=currency_to,
+        value=Decimal("1.1"),
+    )
+    # Act
+    response = client.post(PATH, json=payload.model_dump_json())
+
+    response_model = CalculatedValueDTO.model_validate_json(response.read())
+
+    # Assert
+    assert response_model.value > 0
---
+++ apps/backend/tests/currency/unit/test_http_exchange_rate_repository.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+import re
+from typing import Any, Dict
+import pytest
+from pytest_httpx import HTTPXMock
+from modules.currency.adapters.repositories.exchange_rate_repository.dto.incoming_exchange_rate_dto import (
+    IncomingExchangeRateDTO,
+    RatesDTO,
+)
+
+from modules.currency.adapters.repositories.exchange_rate_repository.http_exchange_rate_repository import (
+    HTTPExchangeRateRepository,
+)
+from modules.currency.core.enums.currency_enum import CurrencyEnum
+from tests.helpers.random import get_random_decimal, get_random_string
+
+import pytest
+
+EXCHANGE_RATE_PATH_REGEXP = "https://api.nbp.pl/api/exchangerates/rates/a/.*"
+
+
+def mock_exchange_rate_http_request(
+    httpx_mock: HTTPXMock,
+    json: Dict[Any, Any],
+    status_code: int = None,
+) -> None:
+    url_pattern = re.compile(EXCHANGE_RATE_PATH_REGEXP)
+    httpx_mock.add_response(url=url_pattern, status_code=status_code or 200, json=json)
+
+
+@pytest.mark.parametrize("currency_enum", list(CurrencyEnum))
+@pytest.mark.asyncio
+async def test_should_get_exchange_rate_when_good_params_provided(
+    httpx_mock: HTTPXMock, currency_enum: CurrencyEnum
+):
+    # Arrange
+    repository = HTTPExchangeRateRepository()
+    exchange_rate = get_random_decimal()
+    payload = IncomingExchangeRateDTO(
+        table=get_random_string(),
+        currency=get_random_string(),
+        code=get_random_string(),
+        rates=RatesDTO(
+            effective_date=datetime.now(),
+            no=get_random_string(),
+            mid=exchange_rate,
+        ),
+    )
+    mock_exchange_rate_http_request(
+        httpx_mock=httpx_mock, json=payload.model_dump_json()
+    )
+
+    # Act
+    result = await repository.get_exchange_rate(currency_enum)
+
+    # Assert
+
+    assert result == exchange_rate

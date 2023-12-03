6d0bb73f111f5e316222278fb42ae6d48c8a845b
Maciej Hankus <maciej.hankus0@gmail.com>
2023-12-03 23:24:06 +0100

    Working docker-compose for api and db

--- apps/backend/src/modules/currency/adapters/api/dto/incoming/calculate_currency_dto.py
+++ apps/backend/src/modules/currency/adapters/api/dto/incoming/calculate_currency_dto.py
@@ -1,10 +1,12 @@
 from decimal import Decimal
 
+from pydantic import Field
+
 from modules.currency.core.enums.currency_enum import CurrencyEnum
 from shared.models.camel_model import CamelModel
 
 
 class IncomingCalculateCurrencyDTO(CamelModel):
-    currency_from: CurrencyEnum 
-    currency_to: CurrencyEnum
-    value: Decimal
+    currency_from: CurrencyEnum = Field(..., description="Base currency you want to exchange." )
+    currency_to: CurrencyEnum = Field(..., description="Target currency." )
+    value: Decimal = Field(..., description="Value of base currency." )
